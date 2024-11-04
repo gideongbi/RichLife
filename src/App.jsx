a6e8c34469bbcc8e6ae71d34b8d2df8a6c815935
Gideon Gyebi <gideon@eduroam-pool7-0675.wlan.uni-bremen.de>
2024-11-04 14:57:22 +0100

    alten zustand wiederhergestellt

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AboutMe from "./components/AboutMe";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
