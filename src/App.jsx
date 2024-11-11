b20f83e43babe5f2255f546ca3f97a735eddfcb4
Gideon Gyebi <gideon@AirvonGideon2.fritz.box>
2024-11-11 21:56:25 +0100

    neue router configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,27 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import AboutMe from "./components/AboutMe";
-import HeroSection from "./components/HeroSection";
+import { useNavigate } from 'react-router-dom'; // useNavigate importieren
+import Home from './components/Home';
 import Navbar from "./components/Navbar";
-import Services from "./components/Services";
-import Contact from "./components/Contact";
-import Cards from "./components/Cards";
 import Footer from "./components/Footer";
-import NavigationButtons from "./components/NavigationButtons";
-import FAQ from "./components/FAQ";
-import Kenstar from "./components/Kenstar"; // Importiere Kenstar
-import AGB from './components/AGB'; // Importiere AGB
-import Datenschutz from './components/Datenschutz'; // Importiere Datenschutz
-import Impressum from './components/Impressum'; // Importiere Impressum
+import Kenstar from "./components/Kenstar"; 
+import AGB from './components/AGB'; 
+import Datenschutz from './components/Datenschutz'; 
+import Impressum from './components/Impressum'; 
 
 const App = () => {
   return (
     <Router>
       <Navbar /> {/* Navbar auf allen Seiten sichtbar */}
-
-      <Routes>
-        {/* Route für die Startseite */}
-        <Route 
-          path="/" 
-          element={
-            <>
-              <HeroSection /> {/* HeroSection nur auf der Startseite */}
-              <Cards />
-              <div className="bg-white">
-                <Services />
-                <AboutMe />
-              </div>
-              <Contact id="contact-section" />
-              <div className="pb-[20vh] pt-[5vh]">
-                <FAQ />
-              </div>
-              <NavigationButtons />
-            </>
-          } 
-        />
-
-        {/* Route für die eigenständige Kenstar-Seite */}
-        <Route path="/kenstar" element={<Kenstar />} />
-        <Route path="/AGB" element={<AGB />} /> {/* Route für AGB-Seite */}
-        <Route path="/Datenschutz" element={<Datenschutz />} /> {/* Route für Datenschutz-Seite */}
-        <Route path="/Impressum" element={<Impressum />} /> {/* Route für Impressum-Seite */}
-      </Routes>
+      
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/kenstar" element={<Kenstar />} />
+          <Route path="/AGB" element={<AGB />} />
+          <Route path="/Datenschutz" element={<Datenschutz />} />
+          <Route path="/Impressum" element={<Impressum />} />
+        </Routes>
+      </div>
 
       {/* Footer bleibt auf allen Seiten */}
       <Footer />
--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/RichLife/',
   plugins: [react()],
+  server: {
+    // Diese Einstellung stellt sicher, dass alle Routen auf index.html zurückfallen
+    historyApiFallback: true
+  }
 })
