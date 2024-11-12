ac1513cb15fa1554220e4bcba502adeee5bbce07
Gideon Gyebi <gideon@AirvonGideon2.fritz.box>
2024-11-12 09:12:12 +0100

    neuer Versuch

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
    base: '/',
-  plugins: [react()],
-  server: {
-    // Diese Einstellung stellt sicher, dass alle Routen auf index.html zurückfallen
-    historyApiFallback: true
-  }
+  plugins: [react()]
 })
