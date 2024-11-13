7fdf8466f37f0a141315e66917e9839110740e2d
Gideon Gyebi <gideon@AirvonGideon2.fritz.box>
2024-11-13 23:26:01 +0100

    burger menp funktioniert

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import logo from "../assets/design.png";
 import { navItems } from "../constants";
 import { Menu, X, Instagram } from "lucide-react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useNavigate, Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTiktok } from '@fortawesome/free-brands-svg-icons';
@@ -19,6 +19,7 @@ const Navbar = () => {
   const handleLinkClick = (path) => {
     navigate(path);
     window.scrollTo(0, 0); // Scrollt die Seite ganz nach oben
+    setMobileDrawerOpen(false); // Schließt das mobile Drawer-Menü
   };
 
   return (
@@ -28,7 +29,7 @@ const Navbar = () => {
           {/* Logo */}
           <div className="flex items-center flex-shrink-0">
             <Link to={"/"}>
-            <img className="h-[6vh] w-[11vh]" src={logo} alt="logo" />
+              <img className="h-[6vh] w-[11vh]" src={logo} alt="logo" />
             </Link>
           </div>
 
@@ -37,11 +38,11 @@ const Navbar = () => {
             {navItems.map((item, index) => (
               <li key={index} className="relative group">
                 <button
-              onClick={() => handleLinkClick(item.href)} // Verwendung der Methode
-              className="text-center relative z-10 group-hover:text-white transition-colors duration-300 px-4"
-            >
-              {item.label}
-            </button>
+                  onClick={() => handleLinkClick(item.href)}
+                  className="text-center relative z-10 group-hover:text-white transition-colors duration-300 px-4"
+                >
+                  {item.label}
+                </button>
                 <div className="absolute inset-0 flex justify-center items-center">
                   <div className="w-full h-full bg-web rounded-full transition-all duration-300 ease-in-out group-hover:scale-110 opacity-0 group-hover:opacity-100"></div>
                 </div>
@@ -76,51 +77,47 @@ const Navbar = () => {
         </div>
 
         {/* Mobile Drawer */}
-{mobileDrawerOpen && (
-  <div className="backdrop-blur-lg bg-white/1 text-white inset-0 h-screen w-screen z-10 flex flex-col items-center lg:hidden">
-    <ul className="w-full h-full flex flex-col items-center justify-evenly py-32">
-      {/* Dynamisches Mapping der navItems */}
-      {navItems.map((item, index) => (
-        <li key={index} className="w-1/3">
-          <Link 
-            to={item.href} 
-            onClick={() => setMobileDrawerOpen(false)}
-            className="block text-center p-2 text-xl bg-web rounded-full" 
-          >
-            {item.label}
-          </Link>
-        </li>
-      ))}
+        {mobileDrawerOpen && (
+          <div className="backdrop-blur-lg bg-white/1 text-white inset-0 h-screen w-screen z-10 flex flex-col items-center lg:hidden">
+            <ul className="w-full h-full flex flex-col items-center justify-evenly py-32">
+              {/* Dynamisches Mapping der navItems */}
+              {navItems.map((item, index) => (
+                <li key={index} className="w-1/3">
+                  <button 
+                    onClick={() => handleLinkClick(item.href)} // Verwendung von handleLinkClick
+                    className=" w-full block text-center p-2 text-xl bg-web rounded-full"
+                  >
+                    {item.label}
+                  </button>
+                </li>
+              ))}
 
-      {/* Instagram Text-Link */}
-      <li className="w-1/3">
-        <a
-          href="https://www.instagram.com/richlife.original?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
-          target="_blank"
-          rel="noopener noreferrer"
-          className="block text-center p-2 text-xl bg-web rounded-full"
-        >
-          Instagram
-        </a>
-      </li>
+              {/* Instagram Text-Link */}
+              <li className="w-1/3">
+                <a
+                  href="https://www.instagram.com/richlife.original?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="block text-center p-2 text-xl bg-web rounded-full"
+                >
+                  Instagram
+                </a>
+              </li>
 
-      {/* TikTok Text-Link */}
-      <li className="w-1/3">
-        <a
-          href="https://www.tiktok.com/@richlife.original"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="block text-center p-2 text-xl bg-web rounded-full"
-        >
-          TikTok
-        </a>
-      </li>
-    </ul>
-  </div>
-)}
-
-
-
+              {/* TikTok Text-Link */}
+              <li className="w-1/3">
+                <a
+                  href="https://www.tiktok.com/@richlife.original"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="block text-center p-2 text-xl bg-web rounded-full"
+                >
+                  TikTok
+                </a>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </nav>
   );
